7ab1ed4a6c9fde9a653626f0ef534088827ec704
Henrique Rezende <henrique.64@hotmail.com>
2024-11-18 23:15:27 -0300

    feat: entities and modules registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { Product } from './products/entities/product.entity';
+import { CartModule } from './cart/cart.module';
+import { CartItemsModule } from './cart-items/cart-items.module';
+import { Cart } from './cart/entities/cart.entity';
+import { CartItem } from './cart-items/entities/cart-item.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: ':memory:',
-      entities: [Product],
+      entities: [Product, Cart, CartItem],
       synchronize: true,
     }),
     ProductsModule,
+    CartModule,
+    CartItemsModule,
   ],
 })
 export class AppModule {}
